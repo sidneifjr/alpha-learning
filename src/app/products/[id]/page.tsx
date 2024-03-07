133d8bccb6f1b9ddf7601850f480f4ee0de63289
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-07 15:22:07 -0300

    feat: add Products page

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,108 @@
+import { Package } from 'lucide-react'
+import Image from 'next/image'
+
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+
+type PageProps = {
+  params: {
+    id: string
+  }
+}
+
+type ProductProps = {
+  id: number
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: string[]
+}
+
+export async function generateMetadata({ params }: PageProps) {
+  return {
+    title: `${params.id}`,
+  }
+}
+
+export default async function Products({ params }: PageProps) {
+  const getProduct = (await fetch(
+    `${process.env.NEXT_API_BASE_URL}/products/${params.id}`
+  ).then((response) => response.json())) as ProductProps
+
+  const productPrice = new Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(getProduct.price)
+
+  return (
+    <section className="pt-16">
+      <div className="mx-auto flex max-w-[1400px] justify-center">
+        <div className="h-[600px] w-[600px]">
+          <Carousel className="h-[600px]">
+            <CarouselContent className="h-[600px]">
+              {getProduct.images.map((image) => {
+                return (
+                  <CarouselItem key={crypto.randomUUID()} className="h-full">
+                    <Image
+                      alt={getProduct.title}
+                      width={660}
+                      height={660}
+                      src={image}
+                      className="object-cover"
+                    />
+                  </CarouselItem>
+                )
+              })}
+            </CarouselContent>
+
+            <div className="relative flex">
+              <CarouselPrevious className="left-0 top-0" />
+              <CarouselNext className="bottom-0 right-0" />
+            </div>
+          </Carousel>
+        </div>
+
+        <div className="flex flex-col pl-6">
+          <span className="tracking-tight text-gray-400">
+            {getProduct.brand}
+          </span>
+
+          <h2 className="text-2xl font-bold tracking-tight">
+            {getProduct.title}
+          </h2>
+
+          <Badge className="tracking-tight">{getProduct.category}</Badge>
+
+          <p className="my-8 flex-1">{getProduct.description}</p>
+
+          <div className="flex flex-1 flex-col gap-4">
+            <div className="flex justify-between">
+              <span className="text-xl tracking-tight text-green-400">
+                {productPrice}
+              </span>
+
+              <span className="flex gap-2">
+                <Package /> {getProduct.stock}
+              </span>
+            </div>
+
+            <Button className="w-full uppercase tracking-tight">Comprar</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
