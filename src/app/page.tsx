747bdce354c69428dc21851fd2d8ff101a9a9af8
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-07 15:18:48 -0300

    feat: add Catalog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { Catalog } from '@/components/catalog'
-import { Header } from '@/components/header'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <Header />
-
       <Catalog />
     </main>
   )
